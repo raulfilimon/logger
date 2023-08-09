40e62fe483119c2a091d8738074cce30a10a6f15
raulfilimon <121848598+raulfilimon@users.noreply.github.com>
2023-08-09 09:46:16 +0300

    replaced Err function with NewErr

--- logger.go
+++ logger.go
@@ -12,38 +12,6 @@ import (
 const layoutISO = "2006-01-02 15:04:05"
 const dateLayout = "2006-01-02"
 
-func Err(err_string string) error {
-	now := time.Now()
-	dateTime := now.Format(layoutISO)
-	date := now.Format(dateLayout)
-
-	cwd, _ := os.Getwd()
-
-	err := os.MkdirAll("storage/logs/", os.ModePerm)
-
-	if err != nil {
-		return err
-	}
-
-	path := filepath.Join(cwd, "storage/logs/", date+".log")
-	newFilePath := filepath.FromSlash(path)
-
-	file, err := os.OpenFile(newFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
-	if err != nil {
-		return err
-	}
-
-	w := bufio.NewWriter(file)
-	_, err = w.WriteString(dateTime + "ERROR: " + err_string + "\n")
-
-	if err != nil {
-		return err
-	}
-
-	w.Flush()
-	return nil
-}
-
 func Info(info string) error {
 	now := time.Now()
 	dateTime := now.Format(layoutISO)
@@ -76,7 +44,7 @@ func Info(info string) error {
 	return nil
 }
 
-func NewErr(_err error) error {
+func Err(_err error) error {
 	now := time.Now()
 	dateTime := now.Format(layoutISO)
 	date := now.Format(dateLayout)
